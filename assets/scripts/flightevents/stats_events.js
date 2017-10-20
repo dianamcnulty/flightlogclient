939bded5caed834932a4c17a6ae269f86e0a4c91
dianamcnulty <dianamcnulty@gmail.com>
2017-10-20 13:45:10 -0400

    add flight stats data
    -flight hours now displays
    -flight count now displays

--- assets/scripts/flightevents/stats_events.js
+++ assets/scripts/flightevents/stats_events.js
@@ -1,1 +1,28 @@
-
+const flightAPI = require('../API/flightapi')
+const setStats = function () {
+  $('#flight-count').text('')
+  $('#site-count').text('')
+  $('#hour-sum').text('')
+  flightAPI.getAllFlights().then((data) => {
+    $('#flight-count').text(data.flights.length)
+    $('#hour-sum').text(calculateHours(data))
+    $('#site-count').text()
+  })
+}
+const calculateHours = function (data) {
+  const flights = data.flights
+  let total = 0
+  console.log(data.flights)
+  for (let i = 0; i < flights.length; i++) {
+    if (flights[i]['duration']) {
+      console.log(flights[i]['duration'])
+      total += flights[i]['duration']
+    // } else {
+    //   console.log('no duration')
+    }
+  }
+  return total
+}
+module.exports = {
+  setStats
+}
