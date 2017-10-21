43f42076668f6f226f0f3e2e2ee306e41e5c24f0
dianamcnulty <dianamcnulty@gmail.com>
2017-10-21 12:47:02 -0400

    Add #sites functionality
    -added calculation for # sites,
    -tested error handling if stat call fails.

--- assets/scripts/flightevents/stats_events.js
+++ assets/scripts/flightevents/stats_events.js
@@ -7,10 +7,10 @@ const setStats = function () {
     .then((data) => {
       $('#flight-count').text(data.flights.length)
       $('#hour-sum').text(calculateHours(data))
-      $('#site-count').text()
+      $('#site-count').text(calculateSites(data))
     })
     .catch(() => {
-      ('#section-alerts').text('Sorry, there was an error retrieving your flight stats. Please try again later.')
+      $('#section-alerts').text('Sorry, there was an error retrieving your flight stats. Please try again later.')
     })
 }
 const calculateHours = function (data) {
@@ -27,6 +27,24 @@ const calculateHours = function (data) {
   }
   return total
 }
+
+const calculateSites = function (data) {
+  const flights = data.flights
+  const siteList = {}
+  for (let i = 0; i < flights.length; i++) {
+    const launch = flights[i]['launch']
+    if (siteList[launch]) {
+      siteList[launch] += 1
+    } else {
+      siteList[launch] = 1
+    }
+  }
+  console.log('site list is', siteList)
+  const totalSites = Object.keys(siteList).length
+  console.log('totalSites is', totalSites)
+  return totalSites
+}
+
 module.exports = {
   setStats
 }
