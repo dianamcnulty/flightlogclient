fa401c3cbdd155799cf680132af6e5601b03645d
dianamcnulty <dianamcnulty@gmail.com>
2017-10-23 09:09:21 -0400

    remove / from prod api origin to test

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'https://pgflightlog.herokuapp.com/'
+    production: 'https://pgflightlog.herokuapp.com'
   }
 }
 
